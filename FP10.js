65101ac1e2fba7056ac160af026d543945c9ce49
Ian Aranha <ianorama@gmail.com>
2018-11-13 21:04:48 -0800

    Generating self made .Filter functions using call backs.

---
+++ FP10.js
@@ -0,0 +1,23 @@
+//Write your own Array.prototype.myFilter(), which should behave exactly like Array.prototype.filter(). You may use a for loop or the Array.prototype.forEach() method.
+
+// the global Array
+var s = [23, 65, 98, 5];
+
+Array.prototype.myFilter = function(callback){
+    var newArray = [];
+    
+    this.forEach(function(a){
+        if(callback(a) === true){
+            newArray.push(a)
+        }
+    })
+        ;
+
+    return newArray;
+};
+
+var new_s = s.myFilter(function(item){
+    return item % 2 === 1;
+});
+
+console.log(new_s)
