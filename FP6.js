01759909a2d5693099094af8621281702ccfed2b
Ian Aranha <ianorama@gmail.com>
2018-11-13 18:04:10 -0800

    Refactoring to remove global variables from all functions.

---
+++ FP6.js
@@ -0,0 +1,51 @@
+//Refactor (rewrite) the code so the global array bookList is not changed inside either function. The add function should add the given bookName to the end of an array. The remove function should remove the given bookName from an array. Both functions should return an array, and any new parameters should be added before the bookName one.
+
+
+
+// the global variable
+var bookList = ["The Hound of the Baskervilles", "On The Electrodynamics of Moving Bodies", "Philosophiæ Naturalis Principia Mathematica", "Disquisitiones Arithmeticae"];
+
+/* This function should add a book to the list and return the list */
+// New parameters should come before the bookName one
+
+//Passing in the List and the name of the book to be added to the list into the add function.
+//Make an exact copy of the array using slice
+//to to copy, push the new book. 
+//This way, the old list is conserved and a new list created.
+function add(arr,bookName) {
+    let newBookList = arr.slice()
+    var addNewBookToList = newBookList.push(bookName)
+    console.log(newBookList)
+    return newBookList;
+}
+
+/* This function should remove a book from the list and return the list */
+// New parameters should come before the bookName one
+
+//Pass the array and the book to be removed into the remove function
+//IF the book is not in the list ==>exit (not added to this program, but should.)
+//If the book is there,
+//Make an exact copy of the array using slice (copy-array).
+//To the copy-array, using splice, create the front-half-array stopping at the index of the book to be removed
+//The copy-array is now shortened by the first few elements which were removed and now the book to be removed is the first element of the copy-array.
+//To the copy-array, using splice, create the back-half-array removing the first element only.
+//concat the front-end-array and the back-end-array
+function remove(arr, bookName) {
+  if (arr.indexOf(bookName) >= 0) {
+    let newerBookList = arr.slice()
+    var frontofBookList = newerBookList.splice(0,arr.indexOf(bookName))
+    console.log(frontofBookList)
+    var endofBookList = newerBookList.splice(1)
+    console.log(endofBookList)
+    newerBookList = frontofBookList.concat(endofBookList)
+    console.log(newerBookList)
+    return newerBookList;
+    // Add your code above this line
+    }
+}
+
+var newBookList = add(bookList, 'A Brief History of Time');
+var newerBookList = remove(bookList, 'On The Electrodynamics of Moving Bodies');
+var newestBookList = remove(add(bookList, 'A Brief History of Time'), 'On The Electrodynamics of Moving Bodies');
+
+console.log(bookList);
